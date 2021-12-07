24a36d71016df1e7d1c5a971fdfed1fd6dec8532
sazzad <bdmamuncse@gmail.com>
2021-12-08 01:41:59 +0600

    Question 3 completed

--- bin/assignment_02_04nov2021_sazzad.dart
+++ bin/assignment_02_04nov2021_sazzad.dart
@@ -3,13 +3,35 @@ void main() {
   // myQuestion1Method();
   // print('#######Question_1_Ends_Here#########');
 
-  print('#######Question_2_Starts_Here#########');
-  myQuestion2Method();
-  print('#######Question_2_Ends_Here#########');
+  // print('#######Question_2_Starts_Here#########');
+  // myQuestion2Method();
+  // print('#######Question_2_Ends_Here#########');
+
+  print('#######Question_3_Starts_Here#########');
+  myQuestion3Method();
+  print('#######Question_3_Ends_Here#########');
 
 
 }
 
+//question 3
+void myQuestion3Method(){
+  List<dynamic> myNewList=[[1,2,3], {'id':110, 'name':'your name'}, 35.2, [], {}, 1, 'Hello flutter'];
+  print('The Length of my New List Before update:${myNewList.length}');
+  print('The Data before Updating the List: $myNewList');
+  myNewList[3]='upd_data1';
+  myNewList[4]={'key':'value'};
+  myNewList.removeAt(6);
+
+  print('The Length of my New List After update:${myNewList.length}');
+
+  myNewList.forEach((element) {
+    print(element);
+  });
+   // print('The Data After Updating List: $myNewList');
+}
+
+//question 2
 void myQuestion2Method(){
   List<String> myNewList=[];
   print('The Length of my New List Before insert:${myNewList.length}');
